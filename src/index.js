a19c526fc945b5c34c61404a38851cf42ba28ce1
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-11-15 18:18:30 -0300

    Update to work with Babel 6

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import fs from 'fs';
 
 const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
 
-export default function ({ Plugin, types: t }) {
-  const VERSION_EXPRESSION = t.literal(version);
-
-  return new Plugin("version-inline", {
+export default function ({ types: t }) {
+  return {
     visitor: {
-      Identifier: {
-        enter: function(node, parent) {
-          if (this.isIdentifier({name: "__VERSION__"})) {
-            return VERSION_EXPRESSION;
-          }
+      ReferencedIdentifier(path) {
+        if (path.node.name === "__VERSION__") {
+          path.replaceWith(t.valueToNode(version));
         }
       }
     }
-  });
+  };
 }
